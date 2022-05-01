fbd153ee4faedbeed61d0d5e5eca6b74cb015f3c
vitoriafeer1042 <96993609+vitoriafeer1042@users.noreply.github.com>
2022-05-01 17:54:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System; //quais os pacotes que ainda vamos utilizar
+
+namespace exercicio001 //agrupador de instruções
+{
+    class Program //modelo de um objeto
+    {
+        static void Main(string[] args) {
+
+            // calculadora
+            /**
+            double d;
+            d = 12 + 3; // soma     
+            d = 12 - 3; // subtração
+            d = 12 * 3;// multiplicação
+            d = 12 / 3; //divisão
+            Console.WriteLine(d);
+            */
+
+            double num1, num2;
+            string tmp;
+            Console.WriteLine("Digite um número:");
+            tmp = Console.ReadLine();
+            num1 = int.Parse(tmp);
+
+            Console.WriteLine("Digite mais um número");
+            tmp = Console.ReadLine();
+            num2 = int.Parse(tmp);
+
+            double resultado;
+            resultado = num1 + num2;
+            Console.WriteLine("Soma:" + resultado);
+
+            resultado = num1 - num2;
+            Console.WriteLine("Subtração:" + resultado);
+
+            resultado = num1 * num2;
+            Console.WriteLine("Multiplicação:" + resultado);
+
+            resultado = num1 / num2;
+            Console.WriteLine("Divisão:" + resultado);
+
+            
+    }
+
+}
+ }
