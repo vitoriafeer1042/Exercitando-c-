d0f09662a4950ea17f4d39d341480cfd28e49ed6
Vitória Fernanda <vitoriafeer1042@gmail.com>
2022-05-02 15:54:36 -0300

    Add files via upload

---
+++ exercicio02.cs
@@ -0,0 +1,4 @@
+﻿Build iniciado...
+1>------ Compilação iniciada: Projeto: exercicio02, Configuração: Debug Any CPU ------
+1>exercicio02 -> C:\Users\Vitória Fernanda\source\repos\exercicio02\exercicio02\bin\Debug\net5.0\exercicio02.dll
+========== Compilar: 1 com êxito, 0 com falha, 0 atualizados, 0 ignorados ==========
